2b6d590be21ea093817cafb909153ca13955d3e2
leedg92 <49174093+leedg92@users.noreply.github.com>
2023-03-07 10:54:56 +0900

    - 테스트(연결 되어있는지만 구현) - assertJ Import 안되는 이슈 해결 - 야믈파일에 h2 디비 연결 주석처리
    * 스타팅임포트 수정

--- board/src/test/java/com/example/board/repository/JpaRepositoryTest.java
+++ board/src/test/java/com/example/board/repository/JpaRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.example.board.repository;
 
 import com.example.board.config.JpaConfig;
+import com.example.board.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
@@ -27,6 +32,14 @@ class JpaRepositoryTest {
     @DisplayName("select 테스트")
     @Test
     void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
 
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(0);
     }
 }
