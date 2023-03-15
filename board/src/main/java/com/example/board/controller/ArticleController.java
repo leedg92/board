efd883e3055e7cb2f3d7d1b205dd9c05f9048a26
leedg92 <49174093+leedg92@users.noreply.github.com>
2023-03-15 10:09:16 +0900

    페이징 기능 추가
    attribute에 넣을 "Article"에는 Page자료형에서 현재 페이지의 정보, 전체 페이지가 자동으로 넘어와서 추가되어있음

--- board/src/main/java/com/example/board/controller/ArticleController.java
+++ board/src/main/java/com/example/board/controller/ArticleController.java
@@ -4,6 +4,9 @@ import com.example.board.domain.type.SearchType;
 import com.example.board.dto.response.ArticleResponse;
 import com.example.board.dto.response.ArticleWithCommentsResponse;
 import com.example.board.service.ArticleService;
+import com.example.board.service.PaginationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,15 +17,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
-    public ArticleController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
 
 
     @GetMapping
@@ -32,7 +36,12 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles",articleService.searchArticles(searchType,searchKeyword,pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchKeyword,pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        System.out.println("asdasdasdasdsa  :::::: " + articles.getNumber());
+        map.addAttribute("articles",articles);
+        map.addAttribute("paginationBarNumbers",barNumbers);
 
         return "articles/index";
     }
--- board/src/main/resources/data.sql
+++ board/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('uno', 'asdf1234', 'Uno', 'uno@mail.com', 'I am Uno.', now(), 'uno', now(), 'uno')
+    ('leedg92', '@dlehd2231', 'leedg92', 'leedg92@mail.com', 'I am leedg92.', now(), 'leedg92', now(), 'leedg92')
 ;
 
 -- 123 게시글
