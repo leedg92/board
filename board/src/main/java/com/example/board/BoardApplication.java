c973b34532e9361f7e985aaa2370be331b2f7b2f
leedg92 <49174093+leedg92@users.noreply.github.com>
2023-04-10 17:42:01 +0900

    jar -> war로 변경 과정에서의 설정 추가

--- board/src/main/java/com/example/board/BoardApplication.java
+++ board/src/main/java/com/example/board/BoardApplication.java
@@ -2,11 +2,18 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(BoardApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
