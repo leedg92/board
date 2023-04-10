598658b100f8b1d1926b08604045e2c6e01aad2a
leedg92 <49174093+leedg92@users.noreply.github.com>
2023-04-10 18:08:47 +0900

    jar로 원복
    * jsp는 리마인드용으로 한번 구현했으니 굳이 배포하지 않아도 될것 같다.
    (요즘 잘 안쓰기도하고)

--- board/src/main/java/com/example/board/BoardApplication.java
+++ board/src/main/java/com/example/board/BoardApplication.java
@@ -2,18 +2,11 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-public class BoardApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-        return application.sources(BoardApplication.class);
-    }
+public class BoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
